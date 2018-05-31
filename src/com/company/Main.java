331d7ad73d81ea043453a6a4a9c61db0f8de39a0
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:52:03 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //int x, y;
+
+        double x = 3.1415;
+        double y = 3.64;
+
+        System.out.println("pi is approximately " + x);
+        System.out.println("My GPA was " + y);
+    }
+}
